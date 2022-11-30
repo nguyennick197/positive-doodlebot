9184f28920438762572dc36911abbddd1d9d8031
Nicholas Nguyen <nick.nguyen@nyu.edu>
2022-11-30 10:07:48 -0800

    feat: use discordjs v13 and reply to doodle command

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 require('dotenv').config();
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
-
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const config = {
+	prefix: "!"
+}
 
 client.login(process.env.CLIENT_TOKEN);
 
+client.on('ready', () => {
+	console.log(`Ready! Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', msg => {
+	if (msg.author.bot) return;
+	if (msg.content.indexOf(config.prefix) !== 0) return;
+	
+	const args = msg.content.slice(config.prefix.length).trim().split(/ +/g);
+	const command = args.shift().toLowerCase();
+
+	if (command !== 'doodle') return;
+
+	msg.reply(`Here is a positive doodle <3`);
+});
